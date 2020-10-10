e5c22f088eea7804ea62fc643c9111bb54822f69
NagalakshmiPandian <72563081+NagalakshmiPandian@users.noreply.github.com>
2020-10-10 20:50:40 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package employee;
+
+public class Main {
+    public static void main(String a[]){
+        Employee e=new Employee();
+        Employee f=new Employee();
+        e.display();
+        System.out.println("The age is "+e.age);
+        System.out.println("The city is "+f.city);
+    }
+}
