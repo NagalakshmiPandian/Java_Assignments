b56a2ad973f33959c72bc44f4b1d3478ff5482fa
NagalakshmiPandian <72563081+NagalakshmiPandian@users.noreply.github.com>
2020-10-10 20:29:27 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,11 @@
+package employee;
+
+public class Employee {
+    String name="Saurabh";
+    int age=23;
+    String city="Chennai";
+      
+    public void display(){
+        System.out.println("The name is "+name);
+    }
+}
